214500bc80c0e7d5994f4171479e67a419129686
Zul-abdin <zulabdin4463@gmail.com>
2020-01-28 18:21:45 -0500

    Lec2:Pointers and Malloc

--- Notes/Lec2/main.c
+++ Notes/Lec2/main.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+
+    /*
+     * Arrays (assume an array named arr)
+     * - Arrays are stored in sequential order in memory
+     * - To mechanically index through an array, to get the element an index, do: index * sizeof(type of array)
+     * - The name of the array is a pointer to the address of the first element in the array in memory, whether it be a[0], a[0][0], ...
+     * - So, doing: arr + (index * sizeof(type of array))
+     * - For a 1-D array: arr IS a pointer, but arr[0] is NOT
+     */
+    char thisIsAString[] = {'s', 't', 'r', 'i', 'n', 'g', '\0'};
+
+    //These both reference the same element
+    // (*) dereferences a memory address
+    // (&) gives the memory address of an element
+    printf("%c\n",thisIsAString[3]);
+    printf("%c\n", *(thisIsAString + 3));
+
+    //These print the same memory address
+    printf("%p\n",&thisIsAString[3]);
+    printf("%p\n",thisIsAString + 3);
+
+    /*
+     * Pointers
+     * - A memory address (of something whose size is known)
+     *   - *n: dereference n (Treat contents of n as a memory address and go to that location, getting the value there)
+     *   - &n: gets address of n
+     */
+
+    char test0[] = "HELLO";
+    char test1[] = "hello";
+
+    char* p = &(test1[0]);
+
+    //Print what p is pointing to
+    printf("%c\n", *p);
+
+    //Set the value at pointer p equal to 'f'
+    //Common error: p = 'f'; (Makes the address p be 'f', treating 'f' as an address rather than a char)
+    *p = 'f';
+
+    printf("%c\n~~~~~~~~~~~~~~~~~~~~~~~~\n", test1[0]);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //Dynamically Allocated Memmory
+    /*
+     * Malloc
+     * - Returns the address of start of allocated memory (void pointer type), and NULL if unsuccessful
+     * - Need to cast malloc-ed memory so that it's type is known
+     * - Need to free(...) afterwards
+     */
+
+    //Linked List node
+    struct _Node{
+        int value;
+        struct _Node* next;
+    };
+    typedef struct _Node Node;
+
+    Node LLhead;
+    LLhead.value=22;
+    LLhead.next = (Node*)malloc(sizeof(Node));
+
     return 0;
 }
