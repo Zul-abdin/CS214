4a9dd2adfa41065ab9af9c302aec0d604c866da1
Zul-abdin <zulabdin4463@gmail.com>
2020-02-13 17:10:33 -0500

    Added function prototypes to Asst0 Added testcase0.txt

--- Projects/Assignment0/filesort.c
+++ Projects/Assignment0/filesort.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int sequentialSort( void* toSort, int (*comparator)(void*, void*)){
-
-    return 0;
-}
-
-int quickSort( void* toSort, int (*comparator)(void*, void*)){
+int sequentialSort(void* toSort, int (*comparator)(void*, void*)){
 
     return 0;
 }
 
-int main() {
+int quickSort(void* toSort, int (*comparator)(void*, void*)){
+
+    return 0;
+}
+
+int intCompare(void* arg1, void* arg2){
+    int ar1 = *(int*)arg1;
+    int ar2 = *(int*)arg2;
+    printf("%d", ar1 - ar2);
+    return ar1 - ar2;
+}
+
+int strCompare(void* arg1, void* arg2){
+    char* ar1 = (char*)arg1;
+    char* ar2 = (char*)arg2;
+    printf("%d", strcmp(ar1, ar2));
+//TODO Replace strcmp() with a self-made string comparing function
+    return strcmp(ar1, ar2);
+}
+
+int main(int argc, char* argv[]) {
     printf("Hello, World!\n");
     return 0;
 }
