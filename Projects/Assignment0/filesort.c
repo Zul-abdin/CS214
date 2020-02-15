f6e2bdeb6d966486f484f385e0febb1c62c29f93
Christopher <christopher.kyong@gmail.com>
2020-02-15 03:10:42 -0500

    Fixed Error checking and added basic implemenation of File Reading for ONLY strings

--- Projects/Assignment0/filesort.c
+++ Projects/Assignment0/filesort.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+typedef struct _stringNode_{
+    char* name;
+    struct _stringNode_* next;
+}stringNode;
+
+typedef struct _numberNode_{
+    int value;
+    struct _numberNode_* next;
+}numberNode;
+
 void readingFile(int fd, char* buffer, int bytesToRead);
-char delimiters[2] = {'\n',','};
+stringNode* tokenCreator(int size);
+stringNode* tokenCreator(int size);
+stringNode* initalization(char* buffer, char* delimiters, int buffersize, int delimitersize);
 
 int sequentialSort(void* toSort, int (*comparator)(void*, void*)){
     int arrayLength = sizeof(toSort)/sizeof(toSort[0]);
@@ -41,35 +58,79 @@ void readingFile(int fd, char* buffer, int bytesToRead){
     int position = 0;
     int bytesRead = 0;
     do{
-        bytesRead += read(fd, buffer[position], bytesToRead-bytesRead);
-        position = bytesRead;
-        if(bytesRead == 0){
-            System.out.println("Finished reading the File");
+        int status = read(fd, &buffer[position], bytesToRead-bytesRead);
+        if(status == 0){
+            printf("Finished reading the File\n");
             break;
+        }else if(status == -1){
+            printf("Error in the file reading\n");
         }
+        bytesRead += status;
+        position = bytesRead; //Position is used to see where to place it in buffer so no overwrites
     }while(bytesRead < bytesToRead);
 }
+
+stringNode* tokenCreator(int size){
+    stringNode* newNode = (stringNode*) malloc(sizeof(stringNode) * 1);
+    newNode->name = (char*) malloc(sizeof(char) * size);
+    memset(newNode->name, '\0', sizeof(char) * size);
+    newNode->next = NULL;
+    return newNode;
+} //Change to generic so it can generate numberNodes
+
+stringNode* initalization(char* buffer, char* delimiters, int buffersize, int delimitersize){
+     int position = 0;
+     int bufferPos;
+     stringNode* head = tokenCreator(20);
+     stringNode* curNode = head;
+     for(bufferPos = 0; bufferPos < (buffersize/sizeof(buffer[0])); ++bufferPos){
+        int temp = 0;
+        int isDelimiter = 0;
+        for(temp = 0; temp < (delimitersize/sizeof(delimiters[0])); ++temp){
+            if(buffer[bufferPos] == delimiters[temp]){
+                isDelimiter = 1;
+                break;
+            }
+        }
+        if(isDelimiter && curNode->name[0] != '\0'){
+            stringNode* newNode = tokenCreator(20);
+            curNode->next = newNode;
+            curNode = newNode;
+            position = 0;
+        }else{
+            curNode->name[position] = buffer[bufferPos];
+            position++; 
+        }
+     }
+    return head;
+} /* To add: make the name of the stringnodes be infinite long. 
+Concept: Double the size of the char[] and memset the characters to the newly doubled array size (Inefficient?)
+
+To Add: Concept for File Reading: READ TILL you find delimiter once find delimiter, store. 
+Currently, file reading will parse only 100 words each.
+ */
 
 int main(int argc, char* argv[]) {
 
     if(argc != 3){
-        System.out.println("Requires exactly 3 arguments. (Executable, sorting, fileToSort)\n");
+        printf("Requires exactly 3 arguments. (Executable, sorting, fileToSort)\n");
         return 0;
     }
 
     if(strlen(argv[1]) != 2 || strcmp(argv[1], "-i") != 0 || strcmp(argv[1], "-q") != 0){
-        System.out.println("Please input only -i or -q!\n");
+        printf("Please input only -i or -q!\n");
         return 0;
     } //NOTE change strcmp once we create the function 
 
     int filedescriptor = open(argv[2], O_RDONLY);
     if(filedescriptor == -1){
-        System.out.println("Error, File does not exist. Please try again!\n");
+        printf("Error, File does not exist. Please try again!\n");
         return 0;
     }
 
+    
     if(close(filedescriptor) < 0){
-        System.out.println("File Descriptor would not close\n");
+        printf("File Descriptor would not close\n");
     }
     return 0;
 }
