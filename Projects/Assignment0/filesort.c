31d9fa1957a751d0b9292ee83c95c7a6b96af7dc
Christopher <christopher.kyong@gmail.com>
2020-02-16 04:39:45 -0500

    Finished the insertion sort parsing, works with both numbers and strings

--- Projects/Assignment0/filesort.c
+++ Projects/Assignment0/filesort.c
@@ -4,9 +4,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <ctype.h>
 
 typedef struct _stringNode_{
-    char* name;
+    char* value;
     struct _stringNode_* next;
     struct _stringNode_* prev;
 }stringNode;
@@ -19,39 +20,106 @@ typedef struct _numberNode_{
 
 void readingFile(int fd, char* buffer, int bytesToRead);
 stringNode* tokenCreator(int size);
-stringNode* tokenCreator(int size);
 stringNode* initalization(char* buffer, char* delimiters, int buffersize, int delimitersize, int filedescriptor, int bytesToRead);
 void printLL(stringNode* head);
-void freeSNode(stringNode* node);
-void printNLL(numberNode* head);
 numberNode* numberCreator();
+void printNLL(numberNode* head);
+void freeSNode(stringNode* node);
 int strcomp(void* arg1, void* arg2);
+int intCompare(void*, void*);
+int insertionSort(void* head, int (*comparator)(void*, void*));
 
-
-int sequentialSort(void* toSort, int (*comparator)(void*, void*)){
-    int arrayLength = sizeof(toSort)/sizeof(toSort[0]); //This will not work since toSort will be a pointer to the start of the array so the size will be 8 instead of the size of the array (FIX LATER)
-    for(int sortedPosition = 1; sortedPosition < arrayLength; ++sortedPosition){
-        int unsortedElementPos = sortedPosition + 1;
-        while(comparator(toSort[unsortedElementPos], toSort[unsortedElementPos - 1]) < 0 && unsortedElementPos > 0){
-            void* temp = toSort[unsortedElementPos - 1];
-            toSort[unsortedElementPos - 1] = toSort[unsortedElementPos];
-            toSort[unsortedElementPos] = temp;
-            unsortedElementPos--;
-        }
-    }
-    return 0;
-}
+int numFile = 0;
 
 int quickSort(void* toSort, int (*comparator)(void*, void*)){
 
     return 0;
 }
 
+int main(int argc, char* argv[]) {
+
+    if(argc != 3){
+        printf("Requires exactly 3 arguments. (Executable, sorting, fileToSort)\n");
+        return 0;
+    }
+
+    if(strlen(argv[1]) != 2 || argv[1][0] != '-' || (argv[1][1] != 'q' && argv[1][1] != 'i')){
+        printf("Please input only -i or -q!\n");
+        return 0;
+    } //NOTE change strcmp once we create the function 
+
+    int filedescriptor = open(argv[2], O_RDONLY);
+    if(filedescriptor == -1){
+        printf("Error, File does not exist. Please try again!\n");
+        return 0;
+    }
+
+    char delimiters[1] = {','};
+    char buffer[100] = {'\0'};
+    int bytesToRead = sizeof(buffer);
+    stringNode* head = initalization(buffer, delimiters, sizeof(buffer), sizeof(delimiters), filedescriptor, bytesToRead);
+    printf("The LL(Tokens) for the file is:\n");
+	printLL(head);
+	
+	
+	numberNode* nhead = NULL;
+
+	if(isdigit(head->value[0]) > 0){
+        numFile = 1;
+		stringNode* curNode = head;
+        numberNode* curNNode = numberCreator();
+
+		head = NULL;
+		nhead = curNNode;
+
+		while(curNode->next != NULL){
+			curNNode->value = atoi(curNode->value);
+
+			numberNode* temp = numberCreator();
+			curNNode->next = temp;
+			temp->prev = curNNode;
+			curNNode = temp;
+
+			stringNode* toFree = curNode;
+			curNode = curNode->next;
+			freeSNode(toFree);
+		}
+	}
+	
+	printf("The LL for the numberNode is:\n");
+	printNLL(nhead);
+	
+	
+	printf("The LL for the stringNode is:\n");
+	printLL(head);
+    
+    int (*compfp)(void*, void*);
+    printf("Sorted:\n");
+    if(numFile){
+        compfp = intCompare;
+        insertionSort(nhead, compfp);
+        printNLL(nhead);
+    }else{
+        compfp = strcomp;
+        insertionSort(head, compfp);
+        printLL(head);
+    }
+
+    if(close(filedescriptor) < 0){
+        printf("File Descriptor would not close\n");
+    }
+    return 0;
+}
+
+void printLL(stringNode* head){
+    while(head != NULL){
+        printf("%s\n", head->value);
+        head = head->next;
+    }
+}
+
 int intCompare(void* arg1, void* arg2){
-    int ar1 = *(int*)arg1;
-    int ar2 = *(int*)arg2;
-    printf("%d", ar1 - ar2);
-    return ar1 - ar2;
+    return (arg1 - arg2);
 }
 
 int strcomp(void* arg1, void* arg2){
@@ -86,8 +154,8 @@ void readingFile(int fd, char* buffer, int bytesToRead){
 
 stringNode* tokenCreator(int size){
     stringNode* newNode = (stringNode*) malloc(sizeof(stringNode) * 1);
-    newNode->name = (char*) malloc(sizeof(char) * size);
-    memset(newNode->name, '\0', sizeof(char) * size);
+    newNode->value = (char*) malloc(sizeof(char) * size);
+    memset(newNode->value, '\0', sizeof(char) * size);
     newNode->next = NULL;
     newNode->prev = NULL;
     return newNode;
@@ -113,7 +181,7 @@ stringNode* initalization(char* buffer, char* delimiters, int buffersize, int de
                 }
             }
 
-            if(isDelimiter && curNode->name[0] != '\0'){
+            if(isDelimiter && curNode->value[0] != '\0'){
                 position = 0;
                 defaultSize = 20;
 
@@ -133,13 +201,13 @@ stringNode* initalization(char* buffer, char* delimiters, int buffersize, int de
                     char* expanded = (char*) malloc(sizeof(char) * defaultSize);
                     memset(expanded, '\0', defaultSize * sizeof(char));
 
-                    memcpy(expanded, curNode->name, defaultSize/2);
+                    memcpy(expanded, curNode->value, defaultSize/2);
 
-                    free(curNode->name);
-                    curNode->name = expanded;
+                    free(curNode->value);
+                    curNode->value = expanded;
                 }
                 if(isspace(buffer[bufferPos]) == 0){
-                    curNode->name[position] = buffer[bufferPos];
+                    curNode->value[position] = buffer[bufferPos];
                     position++; 
                 }
             }
@@ -148,7 +216,7 @@ stringNode* initalization(char* buffer, char* delimiters, int buffersize, int de
         readingFile(filedescriptor, buffer, bytesToRead);
     }
 
-    if(strlen(curNode->name) == 0){
+    if(strlen(curNode->value) == 0){
         if(curNode->prev == NULL){
             head = NULL;
         }else{
@@ -158,88 +226,14 @@ stringNode* initalization(char* buffer, char* delimiters, int buffersize, int de
     }
 
     return head;
-} /* To add: make the name of the stringnodes be infinite long. 
+} /* To add: make the value of the stringnodes be infinite long. 
 Concept: Double the size of the char[] and memset the characters to the newly doubled array size (Inefficient?)
 
 To add: Smart parsing (if it is a number, be able to store it into the numberLL etc.) Currently only works for strings
 To add: removal of whitespace 
-To add: ensure the name[] can be infinitely long (DONE)
+To add: ensure the value[] can be infinitely long (DONE)
 
  */
-
-int main(int argc, char* argv[]) {
-
-    if(argc != 3){
-        printf("Requires exactly 3 arguments. (Executable, sorting, fileToSort)\n");
-        return 0;
-    }
-
-    if(strlen(argv[1]) != 2 || argv[1][0] != '-' || (argv[1][1] != 'q' && argv[1][1] != 'i')){
-        printf("Please input only -i or -q!\n");
-        return 0;
-    } //NOTE change strcmp once we create the function 
-
-    int filedescriptor = open(argv[2], O_RDONLY);
-    if(filedescriptor == -1){
-        printf("Error, File does not exist. Please try again!\n");
-        return 0;
-    }
-
-    char delimiters[1] = {','};
-    char buffer[100] = {'\0'};
-    int bytesToRead = sizeof(buffer);
-    stringNode* head = initalization(buffer, delimiters, sizeof(buffer), sizeof(delimiters), filedescriptor, bytesToRead);
-    printf("The LL(Tokens) for the file is:\n");
-	printLL(head);
-	
-	
-	numberNode* nhead = NULL;
-
-	if(isdigit(head->name[0]) > 0){
-		stringNode* curNode = head;
-        numberNode* curNNode = numberCreator();
-
-		head = NULL;
-		nhead = curNNode;
-
-		while(curNode->next != NULL){
-			curNNode->value = atoi(curNode->name);
-
-			numberNode* temp = numberCreator();
-			curNNode->next = temp;
-			temp->prev = curNNode;
-			curNNode = temp;
-
-			stringNode* toFree = curNode;
-			curNode = curNode->next;
-			freeSNode(toFree);
-		}
-	}
-	
-	printf("The LL for the numberNode is:\n");
-	printNLL(nhead);
-	
-	
-	printf("The LL for the stringNode is:\n");
-	printLL(head);
-    
-
-
-
-
-    if(close(filedescriptor) < 0){
-        printf("File Descriptor would not close\n");
-    }
-    return 0;
-}
-
-void printLL(stringNode* head){
-    while(head != NULL){
-        printf("%s\n", head->name);
-        head = head->next;
-    }
-}
-
 void printNLL(numberNode* node){
     while(node != NULL){
         printf("Value: %d\n", node->value);
@@ -248,7 +242,7 @@ void printNLL(numberNode* node){
 }
 
 void freeSNode(stringNode* node){
-    free(node->name);
+    free(node->value);
     if(node->prev != NULL){
         node->prev->next = node->next;
     }
@@ -263,4 +257,34 @@ numberNode* numberCreator(){
     newNode->next = NULL;
     newNode->prev = NULL;
     return newNode;
+}
+
+int insertionSort(void* head, int (*comparator)(void*, void*)){
+    stringNode* ptr = head;
+    stringNode* temp = NULL;
+    stringNode* valueHolder = NULL;
+
+    if(comparator != &strcomp){
+        ptr = (numberNode*) ptr;
+        temp = (numberNode*) temp;
+        valueHolder = (numberNode*) malloc(sizeof(numberNode) * 1);
+    }else{
+        valueHolder = (stringNode*) malloc(sizeof(stringNode) * 1);
+    }
+    while(ptr != NULL){
+        if(ptr->prev == NULL){ //First element is always sorted
+            ptr = ptr->next;
+            continue;
+        }
+        temp = ptr;
+        while(ptr->prev != NULL && comparator(ptr->value, ptr->prev->value) < 0){
+            valueHolder->value = ptr->value;
+            ptr->value = ptr->prev->value;
+            ptr->prev->value = valueHolder->value;
+            ptr = ptr->prev;
+        }
+        ptr = temp;
+        ptr = ptr->next;
+    }
+    return 1;
 }
