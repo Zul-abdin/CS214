77efff9be7462ae537dde6ab091b397a34acecc3
Zul-abdin <zulabdin4463@gmail.com>
2020-04-14 18:22:29 -0400

    Networking Code Update

--- Notes/Networking/main.c
+++ Notes/Networking/main.c
@@ -75,15 +75,54 @@ int main() {
      *   - Format: Dotted Octets, From 000.000.000 to 255.255.255.255
      * - Port Number - identifies which Process on a given host to relay data/a message to
      *
-     * - ISO OSI: 7-layer stack to generalize networking
-     *   - Application layer: most abstract layer IO
-     *   - Presentation Layer
-     *   - Session Layer: sync info between multiple connections
-     *   - Transport Layer
-     *   - Networking Layer (ex. router): Determine a route
-     *   - Datalink Layer (ex. switches): Point-to-point sending to
-     *   - Physical Layer:
+     * - ISO OSI: 7-layer stack to generalize networking: Higher Layers Use Lower Layers as a service
+     *   <--- HIGHEST LAYER
+     *   - Application layer: Most abstract layer, Interacts with user (ex. UI)
+     *   - Presentation Layer: Modify/Process data to make it useful (ex. en-/de-coding, en-/de-cryption, en-/de-compression)
+     *   - Session Layer: Manages messages over a number of connections (ex. sync, timing, multiplexing, sequencing)
+     *     - Gets information from Transport layer, using it like a service to get info from Data Link Layer, but does not know how transport layer works
+     *   - Transport Layer: first end-to-end Layer, (ex. sockets are transport-layer structures/abstractions)
+     *     - Manages messaging (getting all of message the the other side quickly, reliably, and efficiently)
+     *     - Pieces together information from the Networking Layer and gives it to the session layer requesting information
+     *   - Networking Layer: Determine a route (ex. router)
+     *     - Manages delivery to hosts not directly connected to (builds/maintains routes, relays messages)
+     *   - Data Link Layer: Point-to-point sending to (ex. switches)
+     *     - Manages delivery to hosts directly connected to (error check/encoding, identity)
+     *   - Physical Layer: Manages translation of bits into something physical that propagates, and v.v. (rate, resiliency, bandwidth)
+     *   <--- LOWEST LAYER
      *
+     * - Routing
+     *   - IP Address Design
+     *     - XXX.X.X.XXX
+     *       - The first octet represents a large area
+     *       - Every next octet represents an increasingly smaller amount of area
+     *     - Class Design: Became outdated, didn't separate addresses nicely
+     *       - All octets that are ___ can be set by the owner
+     *       - Class A: 128.___.___.___
+     *       - Class B: 128.5.___.___
+     *       - Class C: 128.5.6.___
+     *       - Class D: 128.5.6.129
+     *       - What if you want 4 addresses? You would need to get 4 completely different class D's...
+     *     - CIDR: Classless InterDomain Routing
+     *       - 128.5.6.129/x where the first x bits are set
+     *       - 128.5.6.129/32 means you only have one address (you can vary the last 2 bits in only 1 way)
+     *       - 128.5.6.129/30 means you have 4 addresses (you can vary the last 2 bits in 4 ways)
+     *       - Now to route a message, you just have to route to the router with the most widest matching bits
+     *       - When routing to far away places, usually the router will build a path to increasingly inexact addresses, from /24 to /10 to /8 ... for example.
+     *       - This inexact router is able to route more of the network.
+     *       - Then eventually you will be at the far supervisor of the router you want, and starting with the far supervisor will go to increasingly exact addresses
+     *     - Tiers of Routing
+     *       - L1
+     *       - L2
+     *       - L3
+     *       - Regional/Organizational (Big ISP)
+     *       - Local (ISP)
+     *       - Personal <-- You
+     *     - Routing Names:
+     *       - DNS (Domain Name Service)
+     *         - Service that runs on top of the internet
+     *         - Translates human-readable names into network-usable IP Addresses
+     *           - Dotted Strings??
      *
      * Special File Descriptors Values
      * - STDIN: 0
